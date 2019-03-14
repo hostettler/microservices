d67f88a65d024346bd909efac2354100a434ccf8
steve.hostettler@gmail.com <Steve.Hostettler@wkfs-frc.local>
2019-03-14 15:49:38 +0100

    Load keycloak configuration on startup, rename financial-app to localhost

--- api-gateway/src/main/docker/config-kong.sh
+++ api-gateway/src/main/docker/config-kong.sh
@@ -9,4 +9,4 @@ curl -S -s -i -X POST  --url http://api-gateway:8001/services/instrument-service
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/valuation-service/routes    --data "paths[]=/api/v1/valuation" 
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/regulatory-service/routes   --data "paths[]=/api/v1/regulatory" 
 #Enable the Open ID Plugin
-curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=oidc" --data "config.client_id=api-gateway" --data "config.client_secret=798751a9-d274-4335-abf6-80611cd19ba1" --data "config.discovery=https%3A%2F%2Ffinancial-app.com%2Fauth%2Frealms%2Fmaster%2F.well-known%2Fopenid-configuration"
+curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=oidc" --data "config.client_id=api-gateway" --data "config.client_secret=798751a9-d274-4335-abf6-80611cd19ba1" --data "config.discovery=https%3A%2F%2Flocalhost%2Fauth%2Frealms%2Fapigw%2F.well-known%2Fopenid-configuration"
