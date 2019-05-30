bc99d9e239cf22c0059bfea1b979962ae74ea759
steve.hostettler@gmail.com <Steve.Hostettler@wkfs-frc.local>
2019-05-30 22:18:11 +0200

    Fix integration tests and coverage report merges unit and integration

---
+++ instrument-service/src/test/java/api/rest/InstrumentRestServiceIT.java
@@ -0,0 +1,30 @@
+package api.rest;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+
+public class InstrumentRestServiceIT {
+
+	@BeforeAll
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:28080/instrument";
+		RestAssured.port = 8080;
+	}
+
+	@Test
+	public void testGetAll() {
+		when().get("/").then().body(containsString("254900LAW6SKNVPBBN21"));
+
+	}
+
+	@Test
+	public void testGet() {
+		when().get("/1").then().body(containsString("254900LAW6SKNVPBBN21"));
+	}
+
+}
