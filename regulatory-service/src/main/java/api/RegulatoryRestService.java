3b9615bc3add7b75eded2bee850a6946deda99fe
Steve Hostettler <Steve.Hostettler@wkfs-frc.local>
2020-03-22 17:50:32 +0100

    add end point to be able to test

--- regulatory-service/src/main/java/api/RegulatoryRestService.java
+++ regulatory-service/src/main/java/api/RegulatoryRestService.java
@@ -1,13 +1,20 @@
 package api;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @ApplicationScoped
 @Path("/regulatory")
 public class RegulatoryRestService {
 
 
-
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAll() {
+		return "regulated";
+	}
 
 }
