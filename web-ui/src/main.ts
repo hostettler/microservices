39c96d7678a260ca3a610f64e8e6e19f52b2ef50
Steve Hostettler <Steve.Hostettler@wkfs-frc.local>
2020-04-11 09:44:10 +0200

    fix confiuration for web-ui

--- web-ui/src/main.ts
+++ web-ui/src/main.ts
@@ -8,7 +8,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { KeycloakService } from './app/services/keycloak/keycloak.service';
 
 if (environment.production) {
   enableProdMode();
