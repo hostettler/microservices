eda241d721f266bdaefe56271f64588d64cebf35
Steve Hostettler <steve.hostettler@wolterskluwer.com>
2021-05-21 14:42:57 +0200

    fix property name

--- web-ui/src/app/pages/e-commerce/status-card/status-card.component.ts
+++ web-ui/src/app/pages/e-commerce/status-card/status-card.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'ngx-status-card',
   styleUrls: ['./status-card.component.scss'],
   template: `
-    <nb-card (click)="on = !on" [ngClass]="{'off': !on}">
+    <nb-card>
       <div class="icon-container">
         <div class="icon status-{{ type }}">
           <ng-content></ng-content>
@@ -12,7 +12,7 @@ import { Component, Input } from '@angular/core';
       </div>
       <div class="details">
         <div class="title h5">{{ title }}</div>
-        <div class="status paragraph-2">{{ on ? 'ON' : 'OFF' }}</div>
+        <div class="status paragraph-2">{{ value }}</div>
       </div>
     </nb-card>
   `,
@@ -21,5 +21,5 @@ export class StatusCardComponent {
 
   @Input() title: string;
   @Input() type: string;
-  @Input() on = true;
+  @Input() value: string;
 }
